d7986720c2a72cf01008354f534feb4fb5b69772
lukassouza <lokasggg27@gmail.com>
2025-10-13 11:46:02 -0300

    Upload metatags

--- app/(auth)/formulario/layout.tsx
+++ app/(auth)/formulario/layout.tsx
@@ -1,27 +1,5 @@
-// app/formulario/layout.tsx
-import "./css/style.css";
-
-import { Inter } from "next/font/google";
-import localFont from "next/font/local";
 import { Metadata } from "next";
 import SeoJsonLd from "@/components/seoJsonLd";
-
-const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-inter",
-    display: "swap",
-});
-
-const nacelle = localFont({
-    src: [
-        { path: "../../public/fonts/nacelle-regular.woff2", weight: "400", style: "normal" },
-        { path: "../../public/fonts/nacelle-italic.woff2", weight: "400", style: "italic" },
-        { path: "../../public/fonts/nacelle-semibold.woff2", weight: "600", style: "normal" },
-        { path: "../../public/fonts/nacelle-semibolditalic.woff2", weight: "600", style: "italic" },
-    ],
-    variable: "--font-nacelle",
-    display: "swap",
-});
 
 export const metadata: Metadata = {
     title: "Formulário de Compra | Som de Rua",
